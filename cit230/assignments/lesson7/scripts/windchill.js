e05887bc1520b5bf88cc5d5885e7ce53d6b11557
Besu2 <lemabesu@gmail.com>
2018-03-01 18:52:53 -0800

    ch8

--- cit230/assignments/lesson7/scripts/windchill.js
+++ cit230/assignments/lesson7/scripts/windchill.js
@@ -1,30 +1,15 @@
-/*var t = 35;
-var s = 5;
-var f = getWindChillFactrs(t, s);
+var h = document.getElementById("h").innerHTML;
+var l = document.getElementById("l").innerHTML;
+var s = document.getElementById("s").innerHTML;
 
-document.getElementById("windSpeed").innerHTML = s;
-document.getElementById("tem").innerHTML = t;
-document.getElementById("windChillFactor").innerHTML = f;
+h = parseInt(h);
+l = parseInt(l);
+s = parseInt(s);
 
-f = 35.74 + 0.6215 t - 35.75 s0.16 + 0.4275 t s0.16;
+var t = (h + l) / 2;
+var w = Math.pow(s, 0.16);
 
-console.log(f);
+var f = 35.74 + 0.6215 * t - 35.75 * w + 0.4275 * t * w;
+f = f.toFixed(2);
 
-function getWindChillFactor(t, s)
-{
-    return
-}
-*/
-function computeNum() {
-	// Input - get input and validate if correct length and if a number
-	var t = 45;
-	var s = 2;
-	
-	// Processing - multiply by the a random number taken to the 3rd power
-	var f = t + Math.pow(2, s);;
-	
-	// Output - output to the div and round the number value to two decimal places
-	get("output").innerHTML = "The windchill is <strong>" + f.toFixed(2) + "</strong>!";
-}
-
-function get(e) { return document.getElementById(e); }
+document.getElementById("windChill").innerHTML = f;
